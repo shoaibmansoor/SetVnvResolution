baca2c40cf6e877a53f06df92c1e583da9e224d1
shoaibm-aurea <shoaib.mansoor@aurea.com>
2021-01-15 21:38:46 +0530

    Update definitions

---
+++ bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const { execSync, spawn } = require('child_process')
+const path = require('path')
+
+function main() {
+    let globalPath = execSync("npm root -g").toString().trim();
+
+    const ps1_path = `${globalPath.trim()}/set-vnc-server-resolution/bin/main.ps1`
+    const child = spawn("powershell.exe",[path.resolve(ps1_path)]);
+    child.stdout.on("data",function(data){
+        console.log("Powershell Data: " + data);
+    });
+    child.stderr.on("data",function(data){
+        console.log("Powershell Errors: " + data);
+    });
+    child.on("exit",function(){
+        console.log("Powershell Script finished");
+    });
+    child.stdin.end()
+}
+
+return main();
